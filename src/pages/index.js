345f1ebcb6b1be8930cfa2d380646c32f187f1a1
Ronaldo Teixeira <ronaldotj2002@yahoo.com.br>
2023-10-26 18:41:58 -0300

    projeto inicial

---
+++ src/pages/index.js
@@ -0,0 +1,14 @@
+import * as React from "react"
+
+const IndexPage = () => {
+  return (
+    <main>
+      <h1>Olá Mundo Gatsby!</h1>
+      <p>Iniciando com os primeiros componentes</p>
+    </main>
+  )
+}
+
+export default IndexPage
+
+export const Head = () => <title>Home Page</title>
---
+++ src/pages/sobre.js
@@ -0,0 +1,14 @@
+import * as React from "react"
+
+const Sobre = () => {
+  return (
+    <main>
+      <h1>Sobre</h1>
+      <p>Iniciando com os primeiros componentes</p>
+    </main>
+  )
+}
+
+export default Sobre;
+
+export const Head = () => <title>Sobre</title>
