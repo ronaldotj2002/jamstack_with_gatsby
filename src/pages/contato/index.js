78bd3b6d6bc041891053c55b565df0c23b699747
Ronaldo Teixeira <ronaldotj2002@yahoo.com.br>
2023-11-11 15:22:38 -0300

    Alteração dos campos no form

--- src/pages/contato/index.js
+++ src/pages/contato/index.js
@@ -10,48 +10,35 @@ const Conato = () => {
   return (
     <Layout pageTitle="Contato">
       <div className="content">
-        <p>Página -  Contato</p>
         <form >
-    <div className="row">
-      <div className="col-25">
-        <label for="fname">First Name</label>
-      </div>
-      <div className="col-75">
-        <input type="text" id="fname" name="firstname" placeholder="Your name.."/>
-      </div>
-    </div>
-    <div className="row">
-      <div className="col-25">
-        <label for="lname">Last Name</label>
-      </div>
-      <div className="col-75">
-        <input type="text" id="lname" name="lastname" placeholder="Your last name.."/>
-      </div>
-    </div>
-    <div className="row">
-      <div className="col-25">
-        <label for="country">Country</label>
-      </div>
-      <div className="col-75">
-        <select id="country" name="country">
-          <option value="australia">Australia</option>
-          <option value="canada">Canada</option>
-          <option value="usa">USA</option>
-        </select>
-      </div>
-    </div>
-    <div className="row">
-      <div className="col-25">
-        <label for="subject">Subject</label>
-      </div>
-      <div className="col-75">
-        <textarea id="subject" name="subject" placeholder="Write something.." ></textarea>
-      </div>
-    </div>
-    <div className="row">
-      <input type="submit" value="Submit"/>
-    </div>
-  </form>
+          <div className="row">
+            <div className="col-25">
+              <label for="nome">Nome</label>
+            </div>
+            <div className="col-75">
+              <input type="text" id="nome" name="nome" placeholder="Nome"/>
+            </div>
+          </div>
+          <div className="row">
+            <div className="col-25">
+              <label for="email">E-mail</label>
+            </div>
+            <div className="col-75">
+              <input type="email" id="email" name="email" placeholder="E-mail"/>
+            </div>
+          </div>          
+          <div className="row">
+            <div className="col-25">
+              <label for="mensagem">Mensagem</label>
+            </div>
+            <div className="col-75">
+              <textarea id="mensagem" name="mensagem" placeholder="Mensagem" ></textarea>
+              <div className="row">
+                <input type="submit" value="Submit"/>
+              </div>
+            </div>
+          </div>
+      </form>
       </div>
     </Layout>
   )
